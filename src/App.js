853f0c7ff2fe413d223a77e93e5ad84f2173c2d9
Josue Castillo <30334352+josuedavi@users.noreply.github.com>
2021-02-08 22:34:45 -0500

    first
    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Header from './header';
+
+class App extends React.Component {
+render() {
+    return (
+      <div className="App">
+        <Header></Header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/header.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+class Header extends React.Component {
+render() {
+    return (
+            <div>
+
+                
+              </div>
+    );
+  }
+}
+
+export default Header;
